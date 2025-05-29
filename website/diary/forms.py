539ce3480d8aadf974547813001f293a86adce54
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-29 23:44:42 +0530

    Initialized form classes for signup and login

--- website/diary/forms.py
+++ website/diary/forms.py
@@ -4,3 +4,12 @@ from datetime import date
 class DiaryForm(forms.Form):
     date = forms.DateField(initial=date.today())
     content = forms.CharField(widget=forms.Textarea())
+
+class SignupForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(widget=forms.PasswordInput())
