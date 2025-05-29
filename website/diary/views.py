31b89a12975edc1c8439191c337fdb2ae145bea8
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-29 23:46:27 +0530

    Added login required for restricting views to only logged in user/ users in session

--- website/diary/views.py
+++ website/diary/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import DiaryForm
 from .models import DiaryModel
 
+@login_required
 def index(request):
     dm = DiaryModel.objects.all()
     if dm:
@@ -16,16 +18,17 @@ def index(request):
                     }
                     )
         return render(request,"diary/index.html",{"context":context})
-    messages.error(request,"No data is available!")
+    messages.error(request,"No data available!")
     return render(request,"diary/index.html")
 
+@login_required
 def write_diary(request):
     if request.method == "POST":
         form = DiaryForm(request.POST)
         if form.is_valid():
             date = form.cleaned_data["date"]
             content = form.cleaned_data["content"]
-            dm = DiaryModel(date=date,content=content)
+            dm = DiaryModel(data=date,content=content)
             dm.save()
             return redirect("index")
     form = DiaryForm()
