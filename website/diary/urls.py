46868d2efa19eff9db124fd28720655744ff248e
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-29 23:45:22 +0530

    Added urlpatterns for signup and login views

--- website/diary/urls.py
+++ website/diary/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from . import auth
 
 urlpatterns=[
     path("",views.index,name="index"),
     path("create/",views.write_diary,name="create"),
+    path("signup/",auth.signup,name="signup"),
+    path("login/",auth.login_page,name="login"),
 ]
