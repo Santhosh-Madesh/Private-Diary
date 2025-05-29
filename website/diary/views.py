ab9dba705a589d30aea278fb7ab582b6b2ef348e
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-29 21:03:29 +0530

    Created write_diary view and passed down the DiaryForm from forms

--- website/diary/views.py
+++ website/diary/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .forms import DiaryForm
 
 def index(request):
     return render(request,"diary/index.html")
 
+def write_diary(request):
+    form = DiaryForm()
+    return render(request,"diary/write_diary.html",{"form":form})
