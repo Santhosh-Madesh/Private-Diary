08f4b5da37c98b3d818c8fa2d0d88f16db959f50
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-29 22:32:39 +0530

    Modified and added logics for writing a diary and display the contents of a diary

--- website/diary/views.py
+++ website/diary/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import DiaryForm
+from .models import DiaryModel
 
 def index(request):
+    dm = DiaryModel.objects.all()
+    if dm:
+        context = []
+        for field in dm:
+            context.append(
+                {
+                    "date":field.date,
+
+                    "content":field.content,
+                    }
+                    )
+        return render(request,"diary/index.html",{"context":context})
+    messages.error(request,"No data is available!")
     return render(request,"diary/index.html")
 
 def write_diary(request):
+    if request.method == "POST":
+        form = DiaryForm(request.POST)
+        if form.is_valid():
+            date = form.cleaned_data["date"]
+            content = form.cleaned_data["content"]
+            dm = DiaryModel(date=date,content=content)
+            dm.save()
+            return redirect("index")
     form = DiaryForm()
     return render(request,"diary/write_diary.html",{"form":form})
