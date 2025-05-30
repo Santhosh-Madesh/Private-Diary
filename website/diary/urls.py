2fb19daf48a91b6a912a87b2f55ddd2f34e43abd
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-05-30 21:27:31 +0530

    Added url pattern for change_password view

--- website/diary/urls.py
+++ website/diary/urls.py
@@ -7,4 +7,5 @@ urlpatterns=[
     path("create/",views.write_diary,name="create"),
     path("signup/",auth.signup,name="signup"),
     path("login/",auth.login_page,name="login"),
+    path("change_password/",auth.change_password,name="change_password"),
 ]
