2be9252a9309afeb98a203efc75ebd25b1376abd
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-01 15:47:00 +0530

    Added a profile model which is in one-one relationship

--- website/diary/models.py
+++ website/diary/models.py
@@ -1,5 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class DiaryModel(models.Model):
     date = models.DateField(unique=True)
     content = models.CharField()
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    age = models.IntegerField()
+    instagram_id = models.CharField()
+    bio = models.CharField()
