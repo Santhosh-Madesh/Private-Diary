9fa9499b971516ca2bc45728c29f3ece0623495d
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-03 13:07:33 +0530

    Final commit

--- website/diary/views.py
+++ website/diary/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import DiaryForm, DashboardForm, DashboardUpdateForm
+from .forms import DiaryForm, DashboardForm, DashboardUpdateForm, ProfileModelForm
 from .models import DiaryModel, Profile
 from django.contrib.auth.models import User
-from django.http import HttpResponse
 
 @login_required
 def index(request):
@@ -41,20 +40,16 @@ def write_diary(request):
 def personal_dashboard(request):
     if request.user.is_authenticated:
         if request.method == "POST":
-            form = DashboardForm(request.POST)
+            form = ProfileModelForm(request.POST, request.FILES)
             if form.is_valid():
-                age = form.cleaned_data["age"]
-                instagram_id = form.cleaned_data["instagram_id"]
-                bio = form.cleaned_data["bio"]
-                p1 = Profile(user = request.user, age = age, instagram_id = instagram_id, bio = bio)
-                p1.save()
+                form.save()
                 return redirect("dashboard")
         profile = Profile.objects.filter(user=request.user)
-        user = User.objects.filter(username=request.user.username)
         if profile:
+            user = User.objects.filter(username=request.user.username)
             return render(request,"diary/dashboard.html",context={"profile":profile,"user":user})
         else:
-            form = DashboardForm()
+            form = ProfileModelForm()
             return render(request,"diary/dashboard_form.html",{"form":form})
     else:
         messages.error(request,"Please log in to access personal dashboard")
@@ -63,31 +58,27 @@ def personal_dashboard(request):
 @login_required
 def update_dashboard(request):
     if request.method == "POST":
-        form = DashboardUpdateForm(request.POST)
+        form = ProfileModelForm(request.POST, request.FILES)
         if form.is_valid():
-            username = form.cleaned_data["username"]
-            email = form.cleaned_data["email"]
             age = form.cleaned_data["age"]
             bio = form.cleaned_data["bio"]
+            instagram_id = form.cleaned_data["instagram_id"]
 
             profile = Profile.objects.get(user=request.user)
             profile.age = age
             profile.bio = bio
+            profile.pfp = request.FILES["pfp"]
             profile.save()
-
-            user = User.objects.get(username = request.user.username)
-            user.email = email
-            user.username = username
-            user.save()
+            
 
             messages.success(request,"Dashboard Successfully Updated!")
             return redirect("dashboard")
 
     profile = Profile.objects.get(user=request.user)
-    username = profile.user.username
-    email = profile.user.email
     age = profile.age
     bio = profile.bio
+    instagram_id = profile.instagram_id
+    pfp = profile.pfp
 
-    form = DashboardUpdateForm(initial={'username':username,'email':email,'age':age,'bio':bio})
+    form = ProfileModelForm(initial={'age':age,'instagram_id':instagram_id,'bio':bio})
     return render(request,"diary/dashboard_update.html",{"form":form})
